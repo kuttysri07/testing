01005554b4a7346c20b2ad6330e6c455fa58642a
Sriram.T <144159691+kuttysri07@users.noreply.github.com>
2024-08-18 12:22:10 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -106,7 +106,7 @@ app.delete("/deleteImage/:id",async (req,res)=>{
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/build","index.html"));
 });
 
 

453223076d3ee2e4de916cccf9bdeecce1990270
Sriram.T <144159691+kuttysri07@users.noreply.github.com>
2024-08-18 12:42:40 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -106,7 +106,7 @@ app.delete("/deleteImage/:id",async (req,res)=>{
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/build","index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
 
 
@@ -115,5 +115,4 @@ app.get("*", (req, res) => {
 const Port = 8000;
 app.listen(Port ,()=>{
     console.log(`server is running on port ${Port}`)
-   
 });
